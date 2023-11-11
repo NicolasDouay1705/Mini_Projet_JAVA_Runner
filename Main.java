1d70a33cdfb259bf5cf2a183913a0f47bc24d780
NicolasDouay17 <146863201+NicolasDouay17@users.noreply.github.com>
2023-11-11 13:22:19 +0100

    Salut

--- Main.java
+++ Main.java
@@ -1,9 +1,23 @@
 import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Hello World");
+        Group root = new Group();
+        Pane pane = new Pane(root);
+        Scene theScene = new Scene(pane,600,400,true);
+        primaryStage.setScene(theScene);
 
+        primaryStage.show();
+    }
+
+    public static void main(String[] args){
+        System.out.println("Hello World");
+        launch(args);
     }
 
 }
